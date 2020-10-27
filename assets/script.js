64bc0aa31a04c64781f00a007bb571b4394755ef
CarolY <mon_alisa75@yahoo.com>
2020-10-27 13:14:17 -0700

    update details.

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,16 @@
+$(document).ready(function () {
+  $("#bioDetail").text(
+    "Hello, my name is Carolina.  Welcome to my portfolio page.  I am a student at coding bootcamp for web development."
+  );
 
-$(document).ready (function() {
-    
-    $("#bioDetail").text("Hello, my name is Carolina.  Welcome to my portfolio page.  I am a student at coding bootcamp for web development.");
-    
-    let a = $("<a>").text("https://github.com/CarolinaYo");
-    let gitHubLink = a.attr("href", "https://github.com/CarolinaYo");
-    let pOne = $("<p>").text("You can find my work at: ").append(gitHubLink);
-    
+  let a = $("<a>").text("https://github.com/CarolinaYo");
+  let gitHubLink = a.attr("href", "https://github.com/CarolinaYo");
+  let pOne = $("<p>").text("You can find my work at: ").append(gitHubLink);
 
-    $("#bioDetail").append(pOne);
+  $("#bioDetail").append(pOne);
 
-
-    let resume =$("<a>").text("Click here to view resume");
-    let pdf = resume.attr("href", "assets/YoedhanaResume_bootcamp2020.pdf");
-    let pTwo = $("<p>").text("Resume: ").append(pdf);
-    $("#contactInfo").append(pTwo);
-
-
-   });
-
+  let resume = $("<a>").text("Click here to view resume");
+  let pdf = resume.attr("href", "assets/YoedhanaResume_bootcamp2020.pdf");
+  let pTwo = $("<p>").text("Resume: ").append(pdf);
+  $("#contactInfo").append(pTwo);
+});
