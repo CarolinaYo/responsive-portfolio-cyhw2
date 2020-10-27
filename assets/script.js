80b9dfe12f127fefe3803c7868a0a516fe7820dd
CarolY <mon_alisa75@yahoo.com>
2020-10-27 11:15:12 -0700

    updated font style and script for contact info.

--- assets/script.js
+++ assets/script.js
@@ -5,13 +5,17 @@ $(document).ready (function() {
     
     let a = $("<a>").text("https://github.com/CarolinaYo");
     let gitHubLink = a.attr("href", "https://github.com/CarolinaYo");
-    let pOne = $("<p>").text("You can find my work repo at: ").append(gitHubLink);
+    let pOne = $("<p>").text("You can find my work at: ").append(gitHubLink);
+    
+
     $("#bioDetail").append(pOne);
 
 
     let resume =$("<a>").text("Click here to view resume");
     let pdf = resume.attr("href", "assets/YoedhanaResume_bootcamp2020.pdf");
-    let pThree = $("<p>").text("Resume: ").append(pdf);
-    $("#contactInfo").append(pThree);
+    let pTwo = $("<p>").text("Resume: ").append(pdf);
+    $("#contactInfo").append(pTwo);
+
+
    });
 
