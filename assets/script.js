0322a4584284a173283051b7424a82658039fad5
CarolY <mon_alisa75@yahoo.com>
2020-10-27 17:17:15 -0700

    cleaned code

--- assets/script.js
+++ assets/script.js
@@ -3,9 +3,9 @@ $(document).ready(function () {
     "Hello, my name is Carolina.  Welcome to my portfolio page.  I am a student at coding bootcamp for web development."
   );
 
-  let a = $("<a>").text("https://github.com/CarolinaYo");
+  let a = $("<a>").text("GitHub");
   let gitHubLink = a.attr("href", "https://github.com/CarolinaYo");
-  let pOne = $("<p>").text("You can find my work at: ").append(gitHubLink);
+  let pOne = $("<p>").text("You can find my work on my ").append(gitHubLink);
 
   $("#bioDetail").append(pOne);
 
