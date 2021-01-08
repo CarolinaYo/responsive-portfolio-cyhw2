60693e4290e78b1f3cda32eb87dc9b93c67b542e
CarolY <mon_alisa75@yahoo.com>
2021-01-08 11:38:10 -0800

    Modify appended paragraph.

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
   $("#bioDetail").text(
-    "Hello, my name is Carolina.  Welcome to my portfolio page.  I am a student at coding bootcamp for web development."
+    "Hello, my name is Carolina."
   );
 
   let a = $("<a>").text("GitHub");
   let gitHubLink = a.attr("href", "https://github.com/CarolinaYo");
   let pOne = $("<p>").text("You can find my work on my ").append(gitHubLink);
 
-  $("#bioDetail").append(pOne);
+  // $("#bioDetail").append(pOne);
 
   let resume = $("<a>").text("Click here to view resume");
   let pdf = resume.attr("href", "assets/YoedhanaResume_bootcamp2020.pdf");
