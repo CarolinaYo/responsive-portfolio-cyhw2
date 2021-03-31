50fe5d96c2a064ada5a6a6a20c05762b557f7823
CarolY <mon_alisa75@yahoo.com>
2021-03-31 09:14:18 -0700

    Updated resume.

--- assets/script.js
+++ assets/script.js
@@ -5,10 +5,10 @@ $(document).ready(function () {
   let gitHubLink = a.attr("href", "https://github.com/CarolinaYo");
   let pOne = $("<p>").text("You can find my work on my ").append(gitHubLink);
 
-  // $("#bioDetail").append(pOne);
+  $("#bioDetail").append(pOne);
 
-  // let resume = $("<a>").text("Click here to view resume");
-  // let pdf = resume.attr("href", "assets/YoedhanaResume_bootcamp2020.pdf");
-  // let pTwo = $("<p>").text("Resume: ").append(pdf);
-  // $("#contactInfo").append(pTwo);
+  let resume = $("<a>").text("Click here to view resume");
+  let pdf = resume.attr("href", "assets/YoedhanaResume_bootcamp2020.pdf");
+  let pTwo = $("<p>").text("Resume: ").append(pdf);
+  $("#contactInfo").append(pTwo);
 });
