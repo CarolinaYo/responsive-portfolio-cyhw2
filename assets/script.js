2bb4065ec42e2491b554fc952f4f643427e4d93f
CarolY <mon_alisa75@yahoo.com>
2021-04-01 10:47:52 -0700

    Improved responsiveness.

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,13 @@
 $(document).ready(function () {
-  $("#bioDetail").text("Hello, my name is Carolina.");
+  // $("#bioDetail").text("Hello, my name is Carolina.");
 
-  let a = $("<a>").text("GitHub");
-  let gitHubLink = a.attr("href", "https://github.com/CarolinaYo");
-  let pOne = $("<p>").text("You can find my work on my ").append(gitHubLink);
+  // let a = $("<a>").text("GitHub");
+  // let gitHubLink = a.attr("href", "https://github.com/CarolinaYo");
+  // let pOne = $("<p>").text("You can find my work on my ").append(gitHubLink);
 
-  $("#bioDetail").append(pOne);
+  // $("#bioDetail").append(pOne);
 
-  let resume = $("<a>").text("Click here to view resume");
+  let resume = $("<a>").text("Resume");
   let pdf = resume.attr("href", "assets/YoedhanaResume_bootcamp2020.pdf");
-  let pTwo = $("<p>").text("Resume: ").append(pdf);
-  $("#contactInfo").append(pTwo);
+  $("#contactInfo").append(pdf);
 });
